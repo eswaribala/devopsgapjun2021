ff8f5d88d2adb72fdf773710713d151d03346aae
eswaribala <parameswaribala@gmail.com>
2021-06-21 12:07:23 +0530

    Modified main

--- ecommerceapp/src/main/java/com/gap/ecommerceapp/EcommerceappApplication.java
+++ ecommerceapp/src/main/java/com/gap/ecommerceapp/EcommerceappApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * 
+ * @author Balasubramaniam
+ *
+ */
 public class EcommerceappApplication {
 
 	public static void main(String[] args) {
